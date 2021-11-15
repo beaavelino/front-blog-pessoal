102b13118ec57009e021af85806978797798d684
beaavelino <beatrizavelino0088@gmail.com>
2021-11-15 15:49:17 -0300

    Postagens

--- blogpessoal/src/app/service/tema.service.ts
+++ blogpessoal/src/app/service/tema.service.ts
@@ -19,8 +19,22 @@ export class TemaService {
     return this.http.get<Tema[]>('https://beatrizavelino.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number):Observable<Tema>{
+    return this.http.get<Tema>(`https://beatrizavelino.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://beatrizavelino.herokuapp.com/tema', tema, this.token)
   }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://beatrizavelino.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://beatrizavelino.herokuapp.com/tema/${id}`, this.token)
+  }
+
+
 
 }
