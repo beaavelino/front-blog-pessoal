c498ec0333818c7f36fe2f3df7764c2ad3e52598
beaavelino <beatrizavelino0088@gmail.com>
2021-12-07 14:06:51 -0300

    Blog

--- blogpessoal/src/app/service/postagem.service.ts
+++ blogpessoal/src/app/service/postagem.service.ts
@@ -19,8 +19,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://beatrizavelino.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://beatrizavelino.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagens(postagem: Postagem) : Observable<Postagem>{
     return this.http.post<Postagem>('https://beatrizavelino.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://beatrizavelino.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://beatrizavelino.herokuapp.com/postagens/${id}`, this.token)
   }
 
 }
